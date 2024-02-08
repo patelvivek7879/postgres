f44ce13e158e60f7a87175ead4d05ad4206adec0
Vivek Patel <patelvivek7879@gmail.com>
2024-02-09 00:05:39 +0530

    Limit clause

---
+++ limit.sql
@@ -0,0 +1,40 @@
+
+
+
+
+
+-- SELECT film_id, title, release_year FROM film ORDER BY film_id LIMIT 5;
+
+
+-- WIth using OFFSET
+-- SELECT film_id, title, release_year FROM film ORDER BY film_id LIMIT 4 OFFSET 3;
+
+
+SELECT film_id, title, rental_rate from film ORDER BY rental_rate DESC LIMIT 10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
